5a65789cdd7eb33f55accef697c327ed7a14db75
Josh Junon <git@josh.junon.me>
2017-04-27 16:33:44 -0700

    use line-centric jello is tasty test

--- test.cc
+++ test.cc
@@ -11,18 +11,19 @@ int main(int argc, char **argv) {
 		? prtty::get("xterm-256color", argv[1])
 		: prtty::get();
 
-	cout
-		<< term.clear_screen
-		<< "hello"
-		<< term.cursor_address(0, 0) << "j"
-		<< term.cursor_address(0, 6) << "is tasty!"
-		<< endl;
-
 	// test boolean operations
 	cout << endl << "has micromovements: " << bool(term.enter_micro_mode) << endl; // usually false
 	cout << "has clear screen: " << bool(term.clear_screen) << endl; // usually true
 	cout << "has set a (ANSI) foreground: " << bool(term.set_a_foreground) << endl; // true on color terminals
 	cout << endl << "colors: " << term.max_colors << endl;
+
+	cout << endl;
+	cout << "hello";
+	cout << term.column_address(); // arguments default to int(0)
+	cout << "j";
+	cout << term.parm_right_cursor(5);
+	cout << "is tasty!";
+	cout << endl;
 
 	return 0;
 }

c01dda027a86fd6113babfa04d3bcbb0ce13959a
Josh Junon <git@josh.junon.me>
2017-04-27 17:41:35 -0700

    show id and term name

--- test.cc
+++ test.cc
@@ -11,10 +11,19 @@ int main(int argc, char **argv) {
 		? prtty::get("xterm-256color", argv[1])
 		: prtty::get();
 
+	cout << "term id: " << term.id << endl;
+	if (!term.names.empty()) {
+		cout << "also known as:" << endl;
+		for (auto &name : term.names) {
+			cout << term.tab << "- " << name << endl;
+		}
+	}
+
 	// test boolean operations
 	cout << endl << "has micromovements: " << bool(term.enter_micro_mode) << endl; // usually false
 	cout << "has clear screen: " << bool(term.clear_screen) << endl; // usually true
 	cout << "has set a (ANSI) foreground: " << bool(term.set_a_foreground) << endl; // true on color terminals
+
 	cout << endl << "colors: " << term.max_colors << endl;
 
 	cout << endl;
